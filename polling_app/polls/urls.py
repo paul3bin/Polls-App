f19862960d2ddadeb2891162ce00f0ccd082e1e3
Ebin Paul <ebinpaul@outlook.com>
2020-08-16 10:40:00 +0530

    update on polling app

--- polling_app/polls/urls.py
+++ polling_app/polls/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import url
 from polls import views
+from django.urls import path
 
 
-urlpatterns = [url(r'^$', views.index, name='polls')]
+urlpatterns = [
+    # path('', views.index, name='index'),
+    url(r'^$', views.index, name='polls'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),]
